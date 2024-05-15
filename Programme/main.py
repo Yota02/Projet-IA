9e8ee907133f922391642b40da7a02755f7009ab
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2024-05-15 09:57:53 +0200

    commentaire et copié coller du git

---
+++ Programme/main.py
@@ -0,0 +1,233 @@
+import cv2
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+import datetime
+from tensorflow import keras
+from tensorflow.keras.models import Model
+import tensorflow as tf
+
+
+url_pikachu = "mettre l'url de l'image" 
+resp = requests.get(url_pikachu, stream=True).raw
+image_array_pikachu = np.asarray(bytearray(resp.read()), dtype="uint8")
+print(f'Shape of the image {image_array_pikachu.shape}')
+image_pikachu = cv2.imdecode(image_array_pikachu, cv2.IMREAD_COLOR)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(image_pikachu, cv2.COLOR_BGR2RGB)) #opencv if BGR color, matplotlib usr RGB so we need to switch otherwise the pikachu will be blue ... O:)
+plt.show()
+
+res = cv2.resize(image_pikachu , dsize=(40,40), interpolation=cv2.INTER_CUBIC)
+print(res.shape)
+res = cv2.cvtColor(res,cv2.COLOR_RGB2GRAY) #TO 3D to 1D
+print(res.shape)
+res = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)[1]
+d = res
+for row in range(0,40):
+    for col in range(0,40):
+        print('%03d ' %d[row][col],end=' ')
+    print('')
+plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
+
+img_bw = cv2.imdecode(image_array_pikachu, cv2.IMREAD_GRAYSCALE)
+(thresh, img_bw) = cv2.threshold(img_bw, 127, 255, cv2.THRESH_BINARY)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(img_bw, cv2.COLOR_BGR2RGB))
+
+kernel = np.matrix([[0,0,0],[0,1,0],[0,0,0]])
+print(kernel)
+img_1 = cv2.filter2D(img_bw, -1, kernel)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(img_1, cv2.COLOR_BGR2RGB))
+
+#defining a vertical edge detection kernel 
+kernel = np.matrix([[-10,0,10],[-10,0,10],[-10,0,10]])
+print(kernel)
+img_1 = cv2.filter2D(img_bw, -1, kernel)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(img_1, cv2.COLOR_BGR2RGB))
+
+#Import dataset
+import pathlib
+import os
+data_dir = tf.keras.utils.get_file(
+    "dataset.zip",
+    "https://github.com/anisayari/Youtube-apprendre-le-deeplearning-avec-tensorflow/blob/master/%234%20-%20CNN/dataset.zip?raw=true",
+    extract=False)
+
+import zipfile
+with zipfile.ZipFile(data_dir, 'r') as zip_ref:
+    zip_ref.extractall('/content/datasets')
+
+data_dir = pathlib.Path('/content/datasets/dataset')
+print(data_dir)
+print(os.path.abspath(data_dir))
+
+image_count = len(list(data_dir.glob('*/*')))
+print(image_count)
+
+batch_size = 3
+img_height = 200
+img_width = 200
+
+train_data = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=42,
+  image_size=(img_height, img_width),
+  batch_size=batch_size,
+  )
+
+val_data = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=42,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = val_data.class_names
+print(class_names)
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_data.take(1):
+  for i in range(3):
+    ax = plt.subplot(1, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+     
+
+from tensorflow.keras import layers
+
+num_classes = 2
+
+model = tf.keras.Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255),
+    layers.Conv2D(128,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(16,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(64,activation='relu'),
+    layers.Dense(num_classes, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'],)
+
+logdir="logs"
+
+from google.colab import files
+file_to_predict = files.upload()
+for file_ in file_to_predict:
+    image_to_predict = cv2.imread(file_,cv2.IMREAD_COLOR)
+    plt.imshow(cv2.cvtColor(image_to_predict, cv2.COLOR_BGR2RGB))
+    plt.show()
+    img_to_predict = np.expand_dims(cv2.resize(image_to_predict,(200,200)), axis=0) 
+    res = model.predict_classes(img_to_predict)
+    print(model.predict_classes(img_to_predict))
+    print(model.predict(img_to_predict))
+    if res == 1:
+        plt.imshow(cv2.cvtColor(image_pikachu, cv2.COLOR_BGR2RGB))
+        plt.show()
+        print("IT'S A PIKACHU !")
+    elif res == 0 :
+        plt.imshow(cv2.cvtColor(image_rondoudou, cv2.COLOR_BGR2RGB))
+        plt.show()
+        print("IT'S A RONDOUDOU !")
+
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir,histogram_freq=1, write_images=logdir,
+                                                   embeddings_data=train_data)
+
+model.fit( 
+    train_data,
+  validation_data=val_data,
+  epochs=2,
+  callbacks=[tensorboard_callback]
+)
+
+def display_image_filtered(name_image,model,layer_name,image):
+    inp= model.inputs 
+    out1= model.get_layer(layer_name).output  
+    feature_map_1= Model(inputs= inp, outputs= out1)  
+    img=cv2.resize(image,(200,200))              
+    input_img= np.expand_dims(img, axis=0)      
+    f=feature_map_1.predict(input_img) 
+    dim = f.shape[3]
+    print(f'{layer_name} | Features Shape: {f.shape}')
+    print(f'Dimension {dim}')
+    fig= plt.figure(figsize=(30,30))
+    if not os.path.exists(f'results_{name_image}'):
+        os.makedirs(f'results_{name_image}')        
+    for i in range(dim):
+        ax = fig.add_subplot(dim/2,dim/2,i+1)
+        ax.axis('off')
+        ax.imshow(f[0,:,:,i])
+        plt.imsave(f'results_{name_image}/{name_image}_{layer_name}_{i}.jpg',f[0,:,:,i])
+
+
+num = 33
+for name in ['pikachu','rondoudou']:
+    if name == 'pikachu':
+        image = image_pikachu
+    elif name == 'rondoudou':
+        image = image_rondoudou
+    plt.axis('off')
+    plt.imshow(cv2.cvtColor(image_pikachu, cv2.COLOR_BGR2RGB))
+    plt.show()
+    for i in range(0,4):
+        if num == 0 and i==0:
+            print('-----------------------------------------------------')
+            print(f'{i+1}st convolutionnal layer')
+            display_image_filtered(name,model,f'conv2d',image)
+            print('--------')
+            print(f'{i-1}nd Pooling')
+            display_image_filtered(name,model,f'max_pooling2d',image)
+            print('-----------------------------------------------------')
+        else:
+            print('-----------------------------------------------------')
+            print(f'{i+1}st convolutionnal layer')
+            display_image_filtered(name,model,f'conv2d_{num+i}',image)
+            print('--------')
+            print(f'{i+1}nd Pooling')
+            display_image_filtered(name,model,f'max_pooling2d_{num+i-1}',image)
+            print('-----------------------------------------------------')
+
+from google.colab import files
+#if there is a bug please be sure pikachu and rondoudou filters are finished to be displayed. If so just relaunch this cell ;)
+!zip -r /content/results_pikachu.zip /content/results_pikachu
+!zip -r /content/results_rondoudou.zip /content/results_rondoudou
+
+files.download("/content/results_pikachu.zip")
+files.download("/content/results_rondoudou.zip")
+     
+def display_filter(model, layer_name):
+    layer = model.get_layer(layer_name)
+    filter, bias= layer.get_weights()
+    dim = filter.shape[3]
+    print(f'{layer_name} | Filter Shape: {filter.shape} Bias Shape: {bias.shape}')
+    print(f'Dimension {dim}')
+    f_min, f_max = filter.min(), filter.max()
+    filter = (filter - f_min) / (f_max - f_min)
+    print(filter.shape)
+    fig= plt.figure(figsize=(30,30))
+    for i in range(dim):
+        ax = fig.add_subplot(dim/2,dim/2,i+1)
+        ax.axis('off')
+        try:
+            ax.imshow(filter[:,:,:,i])
+        except:
+            ax.imshow(filter[:,:,:,i][0])
+            
+display_filter(model,f'conv2d_{num}')
---
+++ Programme/mainaveccom.py
@@ -0,0 +1,166 @@
+import cv2
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+import datetime
+from tensorflow import keras
+from tensorflow.keras.models import Model
+import tensorflow as tf
+
+# URL de l'image de Pikachu
+url_pikachu = "mettre l'url de l'image" 
+resp = requests.get(url_pikachu, stream=True).raw
+image_array_pikachu = np.asarray(bytearray(resp.read()), dtype="uint8")
+print(f'Shape of the image {image_array_pikachu.shape}')
+
+# Décodage de l'image avec OpenCV
+image_pikachu = cv2.imdecode(image_array_pikachu, cv2.IMREAD_COLOR)
+
+# Affichage de l'image avec Matplotlib
+plt.axis('off')
+plt.imshow(cv2.cvtColor(image_pikachu, cv2.COLOR_BGR2RGB)) # Conversion de BGR à RGB
+plt.show()
+
+# Redimensionnement de l'image
+res = cv2.resize(image_pikachu , dsize=(40,40), interpolation=cv2.INTER_CUBIC)
+
+# Conversion en niveaux de gris
+res = cv2.cvtColor(res,cv2.COLOR_RGB2GRAY)
+
+# Binarisation de l'image
+res = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)[1]
+
+# Affichage de l'image binarisée
+plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
+
+# Conversion de l'image en noir et blanc
+img_bw = cv2.imdecode(image_array_pikachu, cv2.IMREAD_GRAYSCALE)
+(thresh, img_bw) = cv2.threshold(img_bw, 127, 255, cv2.THRESH_BINARY)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(img_bw, cv2.COLOR_BGR2RGB))
+
+# Définition du noyau pour la détection de bord vertical
+kernel = np.matrix([[0,0,0],[0,1,0],[0,0,0]])
+print(kernel)
+
+# Application du filtre
+img_1 = cv2.filter2D(img_bw, -1, kernel)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(img_1, cv2.COLOR_BGR2RGB))
+
+# Importation du jeu de données
+import pathlib
+import os
+data_dir = tf.keras.utils.get_file(
+    "dataset.zip",
+    "https://github.com/anisayari/Youtube-apprendre-le-deeplearning-avec-tensorflow/blob/master/%234%20-%20CNN/dataset.zip?raw=true",
+    extract=False)
+
+import zipfile
+with zipfile.ZipFile(data_dir, 'r') as zip_ref:
+    zip_ref.extractall('/content/datasets')
+
+data_dir = pathlib.Path('/content/datasets/dataset')
+print(data_dir)
+print(os.path.abspath(data_dir))
+
+# Comptage du nombre d'images
+image_count = len(list(data_dir.glob('*/*')))
+print(image_count)
+
+# Paramètres pour la création des ensembles de données
+batch_size = 3
+img_height = 200
+img_width = 200
+
+# Création des ensembles de données d'entraînement et de validation
+train_data = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=42,
+  image_size=(img_height, img_width),
+  batch_size=batch_size,
+)
+
+val_data = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=42,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+# Noms de classe
+class_names = val_data.class_names
+print(class_names)
+
+# Affichage des images d'exemple
+plt.figure(figsize=(10, 10))
+for images, labels in train_data.take(1):
+  for i in range(3):
+    ax = plt.subplot(1, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+
+# Définition du modèle CNN
+num_classes = 2
+model = tf.keras.Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255),
+    layers.Conv2D(128,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(16,4, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(64,activation='relu'),
+    layers.Dense(num_classes, activation='softmax')
+])
+
+# Compilation du modèle
+model.compile(optimizer='adam',
+              loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'],)
+
+# Callback pour TensorBoard
+logdir="logs"
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir,histogram_freq=1, write_images=logdir,
+                                                   embeddings_data=train_data)
+
+# Entraînement du modèle
+model.fit( 
+    train_data,
+    validation_data=val_data,
+    epochs=2,
+    callbacks=[tensorboard_callback]
+)
+
+# Fonction pour afficher les filtres de convolution
+def display_filter(model, layer_name):
+    layer = model.get_layer(layer_name)
+    filter, bias= layer.get_weights()
+    dim = filter.shape[3]
+    print(f'{layer_name} | Filter Shape: {filter.shape} Bias Shape: {bias.shape}')
+    print(f'Dimension {dim}')
+    f_min, f_max = filter.min(), filter.max()
+    filter = (filter - f_min) / (f_max - f_min)
+    print(filter.shape)
+    fig= plt.figure(figsize=(30,30))
+    for i in range(dim):
+        ax = fig.add_subplot(dim/2,dim/2,i+1)
+        ax.axis('off')
+        try:
+            ax.imshow(filter[:,:,:,i])
+        except:
+            ax.imshow(filter[:,:,:,i][0])
+
+# Affichage des filtres de convolution
+display_filter(model,f'conv2d_{num}')
